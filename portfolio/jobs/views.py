d26ce5531b98e2a7cae2cb423ea537b467ca6508
LasnamiWalid16 <lasnamiiwalid@gmail.com>
2022-03-01 16:56:22 +0100

    Finishing the urls

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404
 from jobs.models import Job
 
 def AllJobs(request):
@@ -6,3 +6,7 @@ def AllJobs(request):
     jobs = Job.objects.all()
     context={'jobs': jobs}
     return render(request, 'jobs/home.html',context)
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job,pk=job_id)
+    return render(request, 'jobs/detail.html',{'job':job_detail})
